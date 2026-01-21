409a8df119eda9d94cfad75bf157989fb374f3c6
Riccardo <richi-97@live.it>
2026-01-21 16:41:23 +0100

    costruttore getter e setter per Persona

--- src/main/java/ricksciascia/entities/Persona.java
+++ src/main/java/ricksciascia/entities/Persona.java
@@ -20,4 +20,72 @@ public class Persona {
     @Column(name = "gender", nullable = false)
     @Enumerated(EnumType.STRING)
     private Gender gender;
+//    costruttore vuoto per JPA
+    public Persona(){}
+//    costruttore custom
+    public Persona(String nome, String cognome, String email, LocalDate dataDiNascita, Gender gender) {
+        this.nome = nome;
+        this.cognome = cognome;
+        this.email = email;
+        this.dataDiNascita = dataDiNascita;
+        this.gender = gender;
+    }
+//    getter e setter
+
+
+    public long getPersonaId() {
+        return personaId;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getCognome() {
+        return cognome;
+    }
+
+    public void setCognome(String cognome) {
+        this.cognome = cognome;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public LocalDate getDataDiNascita() {
+        return dataDiNascita;
+    }
+
+    public void setDataDiNascita(LocalDate dataDiNascita) {
+        this.dataDiNascita = dataDiNascita;
+    }
+
+    public Gender getGender() {
+        return gender;
+    }
+
+    public void setGender(Gender gender) {
+        this.gender = gender;
+    }
+
+    @Override
+    public String toString() {
+        return "Persona{" +
+                "personaId=" + personaId +
+                ", nome='" + nome + '\'' +
+                ", cognome='" + cognome + '\'' +
+                ", email='" + email + '\'' +
+                ", dataDiNascita=" + dataDiNascita +
+                ", gender=" + gender +
+                '}';
+    }
 }
