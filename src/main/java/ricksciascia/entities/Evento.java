45cd9a3bdb5d85abe347b707bed0f62784c8aefd
Riccardo <richi-97@live.it>
2026-01-21 16:35:45 +0100

    costruttori , getter e setter per evento e location

--- src/main/java/ricksciascia/entities/Evento.java
+++ src/main/java/ricksciascia/entities/Evento.java
@@ -24,4 +24,83 @@ public class Evento {
     @ManyToOne
     @JoinColumn(name="location_id", nullable = false)
     private Location locationEvento;
+
+//    costruttore vuoto per JPA
+    public Evento(){}
+//    costruttore custom
+    public Evento(String titolo,LocalDate dataEvento, String descrizione, TipoEvento tipoEvento, int numeroMassimoPartecipanti, Location locationEvento){
+        this.titolo = titolo;
+        this.dataEvento = dataEvento;
+        this.descrizione = descrizione;
+        this.tipoEvento = tipoEvento;
+        this.numeroMassimoPartecipanti = numeroMassimoPartecipanti;
+        this.locationEvento = locationEvento;
+    }
+//    GETTER e SETTER
+
+
+    public long getId() {
+        return id;
+    }
+
+    public String getTitolo() {
+        return titolo;
+    }
+
+    public void setTitolo(String titolo) {
+        this.titolo = titolo;
+    }
+
+    public LocalDate getDataEvento() {
+        return dataEvento;
+    }
+
+    public void setDataEvento(LocalDate dataEvento) {
+        this.dataEvento = dataEvento;
+    }
+
+    public String getDescrizione() {
+        return descrizione;
+    }
+
+    public void setDescrizione(String descrizione) {
+        this.descrizione = descrizione;
+    }
+
+    public TipoEvento getTipoEvento() {
+        return tipoEvento;
+    }
+
+    public void setTipoEvento(TipoEvento tipoEvento) {
+        this.tipoEvento = tipoEvento;
+    }
+
+    public int getNumeroMassimoPartecipanti() {
+        return numeroMassimoPartecipanti;
+    }
+
+    public void setNumeroMassimoPartecipanti(int numeroMassimoPartecipanti) {
+        this.numeroMassimoPartecipanti = numeroMassimoPartecipanti;
+    }
+
+    public Location getLocationEvento() {
+        return locationEvento;
+    }
+
+    public void setLocationEvento(Location locationEvento) {
+        this.locationEvento = locationEvento;
+    }
+
+    @Override
+    public String toString() {
+        return "Evento{" +
+                "id=" + id +
+                ", titolo='" + titolo + '\'' +
+                ", dataEvento=" + dataEvento +
+                ", descrizione='" + descrizione + '\'' +
+                ", tipoEvento=" + tipoEvento +
+                ", numeroMassimoPartecipanti=" + numeroMassimoPartecipanti +
+                ", locationEvento=" + locationEvento +
+                '}';
+    }
 }
--- src/main/java/ricksciascia/entities/Location.java
+++ src/main/java/ricksciascia/entities/Location.java
@@ -12,7 +12,51 @@ public class Location {
     @Column(name = "location_id")
     private long locationId;
     private String nome;
-    private String città;
+    private String city;
     @OneToMany(mappedBy = "locationEvento")
     private List<Evento> eventi;
+
+//    costruttore vuoto per JPA
+    public Location(){}
+
+//    costruttore custom
+    public Location(String nome, String city){
+        this.nome = nome;
+        this.city = city;
+    }
+//    GETTER e SETTER
+
+
+    public long getLocationId() {
+        return locationId;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getCity() {
+        return city;
+    }
+
+    public void setCity(String city) {
+        this.city = city;
+    }
+
+    public List<Evento> getEventi() {
+        return eventi;
+    }
+
+    @Override
+    public String toString() {
+        return "Location{" +
+                "locationId=" + locationId +
+                ", nome='" + nome + '\'' +
+                ", city='" + city + '\'' +
+                '}';
+    }
 }
